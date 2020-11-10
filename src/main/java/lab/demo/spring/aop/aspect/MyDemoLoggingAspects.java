3a4fd223d3c7981de5d6687ea6a241cc34f0c561
imHarish03 <harishhari302@gmail.com>
2020-11-10 22:22:29 +0530

    Pointcut expression - match method with parameter types Pointcut declaration

--- src/main/java/lab/demo/spring/aop/aspect/MyDemoLoggingAspects.java
+++ src/main/java/lab/demo/spring/aop/aspect/MyDemoLoggingAspects.java
@@ -2,25 +2,31 @@ package lab.demo.spring.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspects {
 
-	//Match on Method names
-	//@Before("execution (public void lab.demo.spring.aop.aspect.MyDemoLoggingAspects.addAccount())")
-	
-	//Match on Method names(using wild card)
-	//@Before("execution (public void add*())")
-	
-	//Use wildcard on modifiers & return types
-	//@Before("execution (* * add*())")
-	
-	//Modifier is optional
-	//@Before("execution (* add*())")
-	
-	@Before("execution (public void addAccount())")
+	// Match on Method names
+	// @Before("execution (public void
+	// lab.demo.spring.aop.aspect.MyDemoLoggingAspects.addAccount())")
+
+	// Match on Method names(using wild card)
+	// @Before("execution (public void add*())")
+
+	// Use wildcard on modifiers & return types
+	// @Before("execution (* * add*())")
+
+	// Modifier is optional
+	// @Before("execution (* add*())")
+
+	@Pointcut("execution(* add*())")
+	private void forDAOPackage() {
+	}
+
+	@Before("forDAOPackage()")
 	public void userLogging() {
 		System.out.println("\n========>> @Before advice on addAccount is called");
 	}
